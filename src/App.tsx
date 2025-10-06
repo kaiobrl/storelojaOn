614f003fa8c4527980beff3772430fa9457c3f3c
[dyad] <git@dyad.sh>
2025-10-06 18:28:12 -0300

    [dyad] Refatorando a estrutura do layout do site - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,26 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import ProductsPage from "./pages/Products";
 import { CartProvider } from "./contexts/CartContext";
-import { Toaster } from "@/components/ui/toaster"
+import { Toaster as SonnerToaster } from "@/components/ui/sonner";
+import Layout from "./components/Layout";
+import { ThemeProvider } from "./components/ThemeProvider";
 
 function App() {
   return (
-    <CartProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/products" element={<ProductsPage />} />
-        </Routes>
-      </BrowserRouter>
-      <Toaster />
-    </CartProvider>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <CartProvider>
+        <BrowserRouter>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="/products" element={<ProductsPage />} />
+            </Routes>
+          </Layout>
+        </BrowserRouter>
+        <SonnerToaster />
+      </CartProvider>
+    </ThemeProvider>
   )
 }
 
-export default App
+export default App;
